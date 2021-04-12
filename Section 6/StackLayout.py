6fdc9a9e9369f5e8adec199e036d0d3642dd6a89
AingHongSin <67510814+AingHongSin@users.noreply.github.com>
2021-04-12 21:53:42 +0700

    Section6 Completed

---
+++ Section 6/StackLayout.py
@@ -0,0 +1,125 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+
+
+class DlgMain(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("QStackLayout")
+        # self.resize(200,200)
+
+        #### Create Widget
+
+        self.cmbSelector = QComboBox()
+        self.cmbSelector.addItems(["General", "Species", "Location", "Surveys"])
+        self.cmbSelector.currentIndexChanged.connect(self.evt_cmbSelector_Changed)
+
+        self.wdgGeneral = QWidget()
+        self.wdgSpecies = QWidget()
+        self.wdgLocation = QWidget()
+        self.wdgSurveys = QWidget()
+
+        #### General Widget
+
+        self.lblNestID = QLabel("34")
+
+        self.dteFound = QDateTimeEdit(QDate(2016, 5, 13))
+
+        self.dteLast = QDateTimeEdit(QDate(2021, 4, 11))
+
+        self.chkActive = QCheckBox()
+
+
+        #### Species Widget
+
+        self.cmbSpecies = QComboBox()
+        self.cmbSpecies.addItem("Red-Tailes Hawk", 300)
+        self.cmbSpecies.addItem("Swainsons Hawk", 400)
+        self.cmbSpecies.addItem("Other", 1600)
+
+        self.ledSpecies = QLineEdit()
+
+        self.spbBuffer = QSpinBox()
+        self.spbBuffer.setValue(800)
+
+
+        #### Location Widget
+
+        self.spbLatitude = QDoubleSpinBox()
+        self.spbLongitude = QDoubleSpinBox()
+
+        #### Survey Widget
+
+        self.lstSurveys = QListWidget()
+        self.lstSurveys.addItem("03/04/2021 - MSM - INACTIVE")
+        self.lstSurveys.addItem("03/30/2021 - MSM - INACTIVE")
+        self.lstSurveys.addItem("04/07/2021 - MSM - INACTIVE")
+        self.lstSurveys.addItem("04/14/2021 - MSM - ACTIVE!!")
+
+        self.btnAddSurvey = QPushButton("Add Survey")
+
+
+        self.setupLayout()
+
+    def setupLayout(self):
+
+        self.lytMain = QHBoxLayout()
+        self.lytLeft = QVBoxLayout()
+        self.lytRight = QStackedLayout()
+
+        self.lytMain.addLayout(self.lytLeft)
+        self.lytMain.addLayout(self.lytRight)
+
+        #### Add Widgets to LeftLayout
+        self.lytLeft.addWidget(self.cmbSelector)
+        self.lytLeft.addStretch()
+
+
+        #### Add Stracked Widgets to rightLayout
+        self.lytRight.addWidget(self.wdgGeneral)
+        self.lytRight.addWidget(self.wdgSpecies)
+        self.lytRight.addWidget(self.wdgLocation)
+        self.lytRight.addWidget(self.wdgSurveys)
+
+        #### Setup General Widget
+        self.lytGeneral = QFormLayout()
+        self.lytGeneral.addRow("Nest ID:", self.lblNestID)
+        self.lytGeneral.addRow("Date Found:", self.dteFound)
+        self.lytGeneral.addRow("Date Lasr Surveyed:", self.dteLast)
+        self.lytGeneral.addRow("Currently Acive", self.chkActive)
+        self.wdgGeneral.setLayout(self.lytGeneral)
+
+        #### Setup Species Widget
+        self.lytSpecies = QFormLayout()
+        self.lytSpecies.addRow("Species:", self.cmbSpecies)
+        self.lytSpecies.addRow("Species:", self.ledSpecies)
+        self.lytSpecies.addRow("Buffer:", self.spbBuffer)
+        self.spbBuffer.setSuffix(" m")
+        self.wdgSpecies.setLayout(self.lytSpecies)
+
+        #### Setup Loaction Widget
+        self.lytLocation = QFormLayout()
+        self.lytLocation.addRow("Latitude:", self.spbLatitude)
+        self.lytLocation.addRow("Longitude:", self.spbLongitude)
+        self.wdgLocation.setLayout(self.lytLocation)
+
+        #### Setup Serveys Widget
+        self.lytSurveys = QVBoxLayout()
+        self.lytSurveys.addWidget(self.lstSurveys)
+        self.lytSurveys.addWidget(self.btnAddSurvey)
+        self.wdgSurveys.setLayout(self.lytSurveys)
+
+        self.setLayout(self.lytMain)
+
+    def evt_cmbSelector_Changed(self, idx):
+        print(idx)
+        self.lytRight.setCurrentIndex(idx)
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    dlgMain = DlgMain()
+    dlgMain.show()
+    sys.exit(app.exec_())
